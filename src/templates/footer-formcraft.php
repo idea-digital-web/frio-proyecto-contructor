b4ff69300aac869a42e06fb57856f97e39b127ea
Alex Ballera <alexballera@gmail.com>
2016-09-15 11:17:39 -0400

    Mejorando footer formcraft

--- src/templates/footer-formcraft.php
+++ src/templates/footer-formcraft.php
@@ -1,5 +1,13 @@
-	<?php if (get_bloginfo( 'url' ) === "http://localhost/frio") {
-	if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); };
-	} else {
-	if (function_exists("add_formcraft_form")) { add_formcraft_form("[fc id='1'][/fc]"); };
-	} ?>
+<?php 
+
+if (function_exists("add_formcraft_form")) {
+	if  (get_bloginfo( 'url' ) === "http://localhost/frio") {
+		add_formcraft_form("[fc id='1'][/fc]");
+	} else { 
+		add_formcraft_form("[fc id='1'][/fc]");
+	}
+} else {
+	echo "<p style='background:red;color:yellow;text-align:center'>AGREGA FORMCRAFT</p>";
+}
+
+ ?>
