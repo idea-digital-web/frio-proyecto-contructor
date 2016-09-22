e2f162a8cb37d815c0ef2f698b176994460195a9
Alex Ballera <alexballera@gmail.com>
2016-09-22 16:35:51 -0400

    Modificado footer

--- src/templates/footer-content.php
+++ src/templates/footer-content.php
@@ -1,18 +1,18 @@
 <div class="site-footer__item">
-	<h2>
+	<h2 class="us">
 	Nosotros
 	</h2>
 	<picture>
 		<img src="http://i0.wp.com/cerroverdestore.com/wp-content/uploads/2016/08/logo-footer.png" alt="Logo Footer" />
 	</picture>
 	<p><?php bloginfo('description'); ?></p>
-	<span class="cards hidden">
+	<span class="cards">
 		<i class="fa fa-cc-visa fa-3x"></i>
 		<i class="fa fa-cc-mastercard fa-3x"></i>
 	</span>
 </div>
-<!-- <div class="site-footer__item">
-	<h2>Categorías</h2>
+<div class="site-footer__item">
+	<h2 class="categories">Categorías</h2>
 	<?php wp_nav_menu(
 			array(
 			'theme_location' => 'primary',
@@ -24,9 +24,9 @@
 			)
 		);
 	?>
-</div> -->
+</div>
 <div class="site-footer__item">
-	<h2>
+	<h2 class="contact-us">
 	Contáctenos
 	</h2>
 	<div class="site-footer__item--contact">
@@ -42,7 +42,7 @@
 		<span><?php get_template_part( 'templates/add', 'whatsapp'); ?></span>
 	</div>
 	<div class="site-footer__item--contact">
-		<!-- <i class="fa fa-envelope"></i> -->
+		<i class="fa fa-envelope"></i>
 		<span class="email"><?php bloginfo('admin_email'); ?></span>
 	</div>
 <!-- 	<div class="site-footer__item--contact">
@@ -50,8 +50,9 @@
 	</div> -->
 </div>
 <div class="site-footer__item">
-	<h2>
+<!-- 	<h2 class="form">
 	Mensaje Directo
-	</h2>
+	</h2> -->
+	<h2 class="form"></h2>
 	<?php get_template_part( 'templates/footer', 'formcraft'); ?>
 </div>
