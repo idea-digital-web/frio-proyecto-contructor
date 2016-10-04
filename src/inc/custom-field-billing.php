9ed98b70f8e27135aec0c785b641617d898ae0db
Alex Ballera <alexballera@gmail.com>
2016-10-03 21:31:52 -0400

    Agregado favicon

--- src/inc/custom-field-billing.php
+++ src/inc/custom-field-billing.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 /**
-https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/ 
-https://surpriseazwebservices.com/edit-woocommerce-checkout-fields/
+ * https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/
+ * https://surpriseazwebservices.com/edit-woocommerce-checkout-fields/
 */
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
