c0a2f3e5dc345123cec97756e244c13855c27240
Alex Ballera <alexballera@gmail.com>
2016-10-04 11:38:42 -0400

    cambiado url mymap

--- src/templates/google-maps.php
+++ src/templates/google-maps.php
@@ -1,2 +1,1 @@
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1dv_J-vKWyXqS_M84Wo3awT48w6w" width="100%" height="480" style="margin-bottom: -5px;"></iframe>
-
+<iframe src="https://www.google.com/maps/d/embed?mid=13874Zt7-yIDIoR0CK2QtycNCsAk" width="100%" height="480" style="margin-bottom: -5px;"></iframe>
