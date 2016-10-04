8f521bc9a99d31b13aa0b91c379738d421c6a5a9
Alex Ballera <alexballera@gmail.com>
2016-10-03 21:37:45 -0400

    Cambiado Idea Pro

--- src/templates/footer-credits.php
+++ src/templates/footer-credits.php
@@ -1,5 +1,5 @@
 <div class="site-footer__credits">
 	<!-- <span><?php bloginfo('name'); ?></span> -->
 	<span>FRIO PROYECTO LLVC C.A J-40040873-3 © 2016</span>
-	<span>Desarrollado por <a href="http://www.ideadigital.com.ve/" target="_blank">Idea Digital</a></span>
+	<span>Desarrollado por <a href="http://www.ideadigital.com.ve/" target="_blank">Idea Pro</a></span>
 </div>
