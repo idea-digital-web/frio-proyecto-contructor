ba73f8eaf2f2bb976590d19104ebea9ecf0ad53e
Alex Ballera <alexballera@gmail.com>
2016-10-06 18:30:18 -0400

    Cambiada imagenes de banners contenido

--- src/templates/banner-content.php
+++ src/templates/banner-content.php
@@ -1,9 +1,11 @@
 <div class="content-area__slides">
   <div>
-    <img src="http://i2.wp.com/cerroverdestore.com/wp-content/uploads/2016/08/banner-content-1.png" alt="Slider Content" />
+    <img src="http://i2.wp.com/frio.ideapruebas.com/blog/wp-content/uploads/2016/10/minibanner-1.jpg" alt="Slider Content" />
   </div>
   <div>
-    <img src="http://i0.wp.com/cerroverdestore.com/wp-content/uploads/2016/08/banner-content-2.png" alt="Slider Content" />
+    <a href="http://frio.ideapruebas.com/servicios/">
+      <img src="http://i0.wp.com/frio.ideapruebas.com/blog/wp-content/uploads/2016/10/minibanner-2.jpg" alt="Slider Content" />
+    </a>
   </div>
   <div>
     <img src="http://i2.wp.com/cerroverdestore.com/wp-content/uploads/2016/08/banner-content-3.png" alt="Slider Content" />
